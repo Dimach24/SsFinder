1debc478f306bcc6b1a53d0adebe3b2ab1e523d0
D.mon 24 <dchaminov@ya.ru>
2024-07-24 23:54:59 +0300

    pss searching faster algo

--- SsFinder.m
+++ SsFinder.m
@@ -1,26 +1,21 @@
 classdef SsFinder
     methods(Static)
-        function [sample_indexes, k_SSB, NId1] = findPss(samples,max_freq_shift,peak_finder)
-            corr_res=zeros(3,max_freq_shift+1,length(samples));
+        function best_match = findPss(samples,max_freq_shift)
+            best_match=struct("max_abs",-1);
             for id=0:2
-                pss=[PssGenerator.generatePssByCellInfo(id) zeros(max_freq_shift)];
-                for freq=0:max_freq_shift
-                    corr_res(id+1,freq+1,:)=xcorr(ifft(circshift(pss,freq)),samples);
+                pss=[PssGenerator.generatePssByCellInfo(id) zeros(1,1920-127)];
+                for freq=(0:max_freq_shift)+56
+                    corr=xcorr(samples,ifft(circshift(pss,freq)));
+                    if best_match.max_abs<max(abs(corr))
+                        best_match.id=id;
+                        best_match.freq=freq;
+                        best_match.corr=corr;
+                        best_match.max_abs=max(abs(corr));
+                    end
                 end
             end
-            absolute=abs(corr_res);
-            m=zeros(3);
-            f_max=zeros(3);
-            for id=1:3
-                [m(id),f_max(id)]=max(absolute(id));
-                m(id)=max(m(id));
-            end
-            [~,id]=max(m);
-            array_with_peaks=absolute(id,f_max(id));
-            NId1=id-1;
-            k_SSB=f_max-57; % -56 -1
-            sample_indexes=peak_finder(array_with_peaks);
-            
+            [~,best_match.lags]=xcorr(samples,pss);
+            best_match.freq=best_match.freq-56;
         end
     end
 end
